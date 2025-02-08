2cf6c8a40083ecc74e529c11a8519add177100b4
Yashaswini200 <110585291+Yashaswini200@users.noreply.github.com>
2025-02-08 13:39:36 +0530

    Delete src/Task0505_5mul.java

--- src/Task0505_5mul.java
+++
@@ -1,9 +0,0 @@
-package Task0502.forloop;
-
-public class Task0505_5mul {
-    public  static void main(String[] args){
-        for(int i=1;i<=10;i++){
-            System.out.printf("5 * %d = %d\n ",i,i*5);
-        }
-    }
-}
