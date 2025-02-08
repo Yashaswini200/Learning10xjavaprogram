5dc99faf24354b3a9a079735dcdc00f4c6408255
Yashaswini200 <110585291+Yashaswini200@users.noreply.github.com>
2025-02-08 13:39:03 +0530

    Delete src/Task0503_hello.java

--- src/Task0503_hello.java
+++
@@ -1,9 +0,0 @@
-package Task0502.forloop;
-
-public class Task0503_hello {
-    public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            System.out.println("Hello");
-        }
-    }
-}
