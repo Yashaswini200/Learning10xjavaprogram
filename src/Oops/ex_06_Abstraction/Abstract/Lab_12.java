c33a4cf13de41460ba6311f0668f21d1318649b7
Yashaswini200 <yashusuresh2050@gmail.com>
2025-02-24 21:58:46 +0530

    commit

---
+++ src/Oops/ex_06_Abstraction/Abstract/Lab_12.java
@@ -0,0 +1,9 @@
+package Oops.ex_06_Abstraction.Abstract;
+
+public class Lab_12 {
+    public static void main(String[] args) {
+        Engine en=new Engine();
+        en.drive();
+    }
+
+}
---
+++ src/Oops/ex_06_Abstraction/Interface/Lab_13.java
@@ -0,0 +1,11 @@
+package Oops.ex_06_Abstraction.Interface;
+
+public class Lab_13 {
+    public static void main(String[] args) {
+        Car c1=new Car();
+        c1.start();
+        c1.stop();
+        c1.tyre();
+        c1.run();
+    }
+}
