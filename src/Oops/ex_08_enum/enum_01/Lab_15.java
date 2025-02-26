19717643662ccdfcb63b216cc8f83f49a9894cc8
Yashaswini200 <yashusuresh2050@gmail.com>
2025-02-26 22:11:39 +0530

    commit

---
+++ src/Oops/ex_08_enum/enum_01/Lab_15.java
@@ -0,0 +1,12 @@
+package Oops.ex_08_enum.enum_01;
+
+public class Lab_15 {
+
+        public static void main(String[] args) {
+
+            System.out.println(Week.saturday);
+
+        }
+    }
+
+
