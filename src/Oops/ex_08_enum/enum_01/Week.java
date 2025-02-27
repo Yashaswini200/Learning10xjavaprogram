72ffbead9ff4e932103cce4f678991185027d689
Yashaswini200 <yashusuresh2050@gmail.com>
2025-02-27 22:07:09 +0530

    commit

---
+++ src/Oops/ex_08_enum/enum_01/Week.java
@@ -0,0 +1,5 @@
+package Oops.ex_08_enum.enum_01;
+
+public enum Week {
+Sunday,monday,tuesday,wednesday,thrusday,friday,saturday;
+}
---
+++ src/Oops/ex_08_enum/enum_02/Lab_15.java
@@ -0,0 +1,7 @@
+package Oops.ex_08_enum.enum_02;
+
+public class Lab_15 {
+    public static void main(String[] args) {
+        System.out.println(Color.blue.gethexcod());
+    }
+}
