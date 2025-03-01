260c782e240271ed5a274dfca657b1fe87da58d7
Yashaswini200 <yashusuresh2050@gmail.com>
2025-03-01 22:27:10 +0530

    commit

---
+++ src/Oops/ex_06_Abstraction/Abstract/Wragon.java
@@ -0,0 +1,9 @@
+package Oops.ex_06_Abstraction.Abstract;
+
+abstract class Wragon {
+    abstract void startengine();
+    void stopengine(){
+        System.out.println("stop the engine");
+    }
+
+}
