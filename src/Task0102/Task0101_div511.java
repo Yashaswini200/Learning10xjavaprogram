c86878382ebbc20deebf1c818dd020c17a983122
Yashaswini <110585291+Yashaswini200@users.noreply.github.com>
2025-05-15 21:07:32 +0530

    Update Task0101_div511.java

--- src/Task0102/Task0101_div511.java
+++ src/Task0102/Task0101_div511.java
@@ -11,6 +11,7 @@ public class Task0101_div511 {
             System.out.println(n+" is divisible by 5 and 11");
         }else {
             System.out.println(n+" is not divisible by 5 and 11");
+            //
         }
     }
 }
